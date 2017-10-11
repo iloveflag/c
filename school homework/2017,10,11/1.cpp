2bdb60aebcda8a05fa5345cba87dcf509639982b
醉梦半醒 <1542980987@qq.com>
2017-10-11 11:02:42 +0800

    作业上传

---
+++ school homework/2017,10,11/1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int a,b;
+	printf("请输入一个整数。\na=");
+	scanf("%d",&a);
+	printf("请输入一个整数。\nb=");
+	scanf("%d",&b);
+	printf("a,b互换后为a=%d,b=%d",b,a);
+	
+	
+}
+/*1.输入a，b的值，然后将a，b的值交换，<即如果输入时a=2，b=3，则执行完后变成a=3，b-2；>*/ 
