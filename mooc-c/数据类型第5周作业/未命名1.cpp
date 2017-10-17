5d63cc2edb9c79d9a0600b74143722338bc5647c
醉梦半醒 <1542980987@qq.com>
2017-10-17 22:32:27 +0800

    shangchuan

--- mooc-c/数据类型第5周作业/未命名1.cpp
+++ mooc-c/数据类型第5周作业/未命名1.cpp
@@ -1,17 +1,18 @@
 #include <stdio.h>
 int main(){
-	int a,b;
-	scanf("%d/%d",&a,&b);
+	int a=16,b=19;
+	//scanf("%d/%d",&a,&b);
 	if(10<=a&&a<b&&b<100){
+		printf("0.") ;
 		int cnt=0;
 		a=a*10;
-			for(;cnt<201;cnt++){
-					printf("%d",a/b);
-			a=a/b*10;
-		
 			
-			printf("\n");
-			}		
+				for(;cnt<201;cnt++){
+					if(a%b!=0){
+					printf("%d",a/b);
+					a=a/b*10;
+					}
+				}	
 	}
 }
 	
---
+++ school homework/2017.10.17/zrq.c
@@ -0,0 +1,57 @@
+/*main()
+{
+int x,t;
+scanf("%d",&x);
+t=x>3&&x<15;
+printf("%d",t);
+
+}*/
+
+
+
+/*main()
+{
+char ch;
+int t;
+scanf("%c",&ch);
+
+t=(ch>'a'&&ch<'z')||(ch>'A'&&ch<'Z');
+printf("%d",t);
+}*/
+
+
+
+/*main(){
+int a,b,c,t;
+scanf("%d %d %d",&a,&b,&c);
+t=a>b&&a>c;
+printf("%d",t);
+}*/
+
+
+/*main()
+{
+int m,n,t;
+scanf("%d %d",&m,&n);
+t=m%n==0;
+printf("%d",t);
+}*/
+
+
+
+/*main(){
+int x,t;
+scanf("%d",x);
+t=(x>1&&x<10)||(x>20&&x<30);
+printf("%d",t);
+}*/
+
+
+
+/*main()
+{
+int a,x,y,t;
+scanf("%d %d %d",&a,&x,&y);
+t=(a%x==0)&&(a%y==0);
+printf("%d",t);
+}*/
